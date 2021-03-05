21075504200c36b5dcb6fb196ec5e445b9d293a9
Yash Roongta <yash12392@gmail.com>
2021-03-05 12:16:15 +0530

    changed full_bhavcopy_save to bhavcopy_save
    full_bhavcopy_save data is not supported by NSE anymore for dates prior to 2020.

--- codes_on_blog/bhavcopy_downloader.py
+++ codes_on_blog/bhavcopy_downloader.py
@@ -7,7 +7,7 @@ The user is also expected to give the correct dates and holidays, read comments 
 
 #Making all necessary imports for the code
 from datetime import date
-from jugaad_data.nse import full_bhavcopy_save
+from jugaad_data.nse import bhavcopy_save
 import pandas as pd
 from jugaad_data.holidays import holidays
 from random import randint
@@ -26,12 +26,12 @@ dates_list = [x.date() for x in date_range]
 
 for dates in dates_list:
      try:
-          full_bhavcopy_save(dates, savepath)
+          bhavcopy_save(dates, savepath)
           time.sleep(randint(1,4)) #adding random delay of 1-4 seconds
      except (ConnectionError, ReadTimeoutError) as e:
           time.sleep(10) #stop program for 10 seconds and try again.
           try:
-               full_bhavcopy_save(dates, savepath)
+               bhavcopy_save(dates, savepath)
                time.sleep(randint(1,4))
           except (ConnectionError, ReadTimeoutError) as e:
                print(f'{dates}: File not Found')
